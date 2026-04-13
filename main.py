a8018b875f2c5ef5ac8c66c0f6a532f55eff6cdd
DoonyFreeman <rebrikov2006@gmail.com>
2026-04-13 22:07:07 +0300

    addited stress_test

--- main.py
+++ main.py
@@ -1,44 +1,21 @@
 from fastapi import FastAPI, Query, Body
 import uvicorn
-import threading
 
-app = FastAPI( )
-
-
-
+app = FastAPI()
 
 hotels = [
     {"id": 1, "title": "Sochi", "name": "soci"},
     {"id": 2, "title": "Krim", "name": "krim"},
     {"id": 3, "title": "Anapa", "name": "anapa"},
-    {"id": 4 , "title": "Surgut", "name": "surgut"},
+    {"id": 4, "title": "Surgut", "name": "surgut"},
 ]
-import time
-import asyncio
-
-@app.get("/sync/{id}")
-def sync_func(id: int):
-    print(f"sync ПОТОКОВ: {threading.active_count()}")
-    print(f"sync Начал {id}: {time.time():.2f}")    
-    time.sleep(3)
-    print(f"sync Закончил {id}: {time.time():.2f}")
-
-@app.get("/async/{id}")
-async def async_func(id: int):
-    print(f"async ПОТОКОВ: {threading.active_count()}")
-    print(f"async Начал {id}: {time.time():.2f}")
-    await asyncio.sleep(3)
-    print(f"async Закончил {id}: {time.time():.2f}")
-
-
-
-
 
 
 @app.get("/hotels")
-def get_hotels(id: int | None = Query(None, description="id отеля"),
-               title: str | None = Query(None, description="Название отеля"),
-               ):
+def get_hotels(
+    id: int | None = Query(None, description="id отеля"),
+    title: str | None = Query(None, description="Название отеля"),
+):
     hotels_ = []
     for hotel in hotels:
         if id and hotel["id"] != id:
@@ -49,16 +26,16 @@ def get_hotels(id: int | None = Query(None, description="id отеля"),
     return hotels_
 
 
-
 @app.post("/hotels")
-def create_hotel(title: str = Body(embed=True,)):
+def create_hotel(
+    title: str = Body(
+        embed=True,
+    ),
+):
     global hotels
-    hotels.append(
-        {"id": hotels[-1]["id"] + 1,
-         "title": title
-         }
-    )
+    hotels.append({"id": hotels[-1]["id"] + 1, "title": title})
     return {"Status": "OK"}
+
 
 @app.put("/hotels/{hotel_id}")
 def put_hotel(hotel_id: int, title: str = Body(), name: str = Body()):
@@ -70,12 +47,17 @@ def put_hotel(hotel_id: int, title: str = Body(), name: str = Body()):
             return {"status": "OK", "hotel": hotel}
     return {"status": "error", "message": "Hotel not found"}
 
+
 @app.patch(
-        "/hotels/{hotel_id}", 
-        summary="Частичное обновление данных об отеле",
-        description="Тут мы частично обновляем данные об отеле: можно отправить name, а можно title, а можно ничего"
-        )
-def patch_hotel(hotel_id: int, title: str | None = Body(default=None), name: str | None = Body(default=None)):
+    "/hotels/{hotel_id}",
+    summary="Частичное обновление данных об отеле",
+    description="Тут мы частично обновляем данные об отеле: можно отправить name, а можно title, а можно ничего",
+)
+def patch_hotel(
+    hotel_id: int,
+    title: str | None = Body(default=None),
+    name: str | None = Body(default=None),
+):
     global hotels
     for hotel in hotels:
         if hotel["id"] == hotel_id:
@@ -84,7 +66,6 @@ def patch_hotel(hotel_id: int, title: str | None = Body(default=None), name: str
             if name is not None:
                 hotel["name"] = name
             return {"status": "ok", "hotel": hotel}
-    
 
 
 @app.delete("/hotels/{hotel_id}")
@@ -94,16 +75,14 @@ def delete_hotel(hotel_id: int):
     return {"Status": "OK"}
 
 
-
-
-
-
 @app.get("/")
 def func():
     return "Hello world!"
 
 
-
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload = False, workers=1,)
-
+    uvicorn.run(
+        "main:app",
+        reload=False,
+        workers=1,
+    )
