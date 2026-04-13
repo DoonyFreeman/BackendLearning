b46d42f0515f666e6f63474986b8a5b6265b5633
DoonyFreeman <rebrikov2006@gmail.com>
2026-04-13 22:00:28 +0300

    edited async

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Query, Body
 import uvicorn
+import threading
 
 app = FastAPI( )
 
@@ -13,8 +14,21 @@ hotels = [
     {"id": 4 , "title": "Surgut", "name": "surgut"},
 ]
 import time
+import asyncio
 
+@app.get("/sync/{id}")
+def sync_func(id: int):
+    print(f"sync ПОТОКОВ: {threading.active_count()}")
+    print(f"sync Начал {id}: {time.time():.2f}")    
+    time.sleep(3)
+    print(f"sync Закончил {id}: {time.time():.2f}")
 
+@app.get("/async/{id}")
+async def async_func(id: int):
+    print(f"async ПОТОКОВ: {threading.active_count()}")
+    print(f"async Начал {id}: {time.time():.2f}")
+    await asyncio.sleep(3)
+    print(f"async Закончил {id}: {time.time():.2f}")
 
 
 
@@ -91,5 +105,5 @@ def func():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload = True)
+    uvicorn.run("main:app", reload = False, workers=1,)
 
