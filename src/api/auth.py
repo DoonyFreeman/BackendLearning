f4963adb843797267a1511f280aa2b66cb94ab3c
DoonyFreeman <rebrikov2006@gmail.com>
2026-04-21 18:24:12 +0300

    added hash

--- src/api/auth.py
+++ src/api/auth.py
@@ -17,7 +17,7 @@ async def register_user(
     hashed_password = pwd_context.hash(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
-        user = await UsersRepository(session).add(new_user_data)
+        await UsersRepository(session).add(new_user_data)
         await session.commit()
 
     return {"status": "OK"}
