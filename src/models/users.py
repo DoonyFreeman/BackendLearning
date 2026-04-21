e0a57fbda5b62cd00981c836fb12017e718afb45
DoonyFreeman <rebrikov2006@gmail.com>
2026-04-21 15:43:24 +0300

    added model + migration "users "

---
+++ src/models/users.py
@@ -0,0 +1,14 @@
+from src.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer
+
+
+class UsersOrm(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(200))
+    hashed_password: Mapped[str] = mapped_column(String(200))
+
+
+    
