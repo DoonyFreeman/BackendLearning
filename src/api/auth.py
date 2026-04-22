abd48e12d8c8216ea375bdcc9f5ddebf27546196
DoonyFreeman <rebrikov2006@gmail.com>
2026-04-22 14:13:00 +0300

    added exceptions

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response
 
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
@@ -13,6 +13,10 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
@@ -41,11 +45,19 @@ async def register_user(
 @router.post("/login")
 async def login_user(
     data: UserRequestAdd,
+    response: Response
 ):
+    
+
     async with async_session_maker() as session:
-        user = await UsersRepository(session).get_one_or_none(email=data.email)
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
         if not user:
-            raise HTTPException(status_code=400, detail="Пользователь с таким email не найден")
+            raise HTTPException(status_code=401, detail="Пользователь с таким email не найден")
+        
+        if not verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Неверный пароль")
+
         access_token = create_access_token({"user_id": user.id})
+        response.set_cookie(key="access_token", value=access_token)
         return {"access_token": access_token}
     
