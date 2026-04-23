36f9c1f4826c7a918deafc6d21739f2cb09bbaf7
DoonyFreeman <rebrikov2006@gmail.com>
2026-04-24 02:09:04 +0300

    added logout router

--- src/api/auth.py
+++ src/api/auth.py
@@ -68,3 +68,10 @@ async def get_me(
         user = await UsersRepository(session).get_one_or_none(id=user_id)
         return user
 
+
+@router.get("/logout")
+async def logout(
+    response: Response
+):
+    response.delete_cookie("access_token")
+    return {"status": "OK"}
