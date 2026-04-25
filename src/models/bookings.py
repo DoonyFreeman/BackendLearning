cc911f6e7ee30a1769cddd0b568936e4497068e3
DoonyFreeman <rebrikov2006@gmail.com>
2026-04-25 16:01:17 +0300

    added booking model and migration

---
+++ src/models/bookings.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.hybrid import hybrid_property
+from datetime import date
+from src.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer,ForeignKey
+
+
+class BookingsOrm(Base):
+    __tablename__ = "bookings"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
+    date_from: Mapped[date]
+    date_to: Mapped[date]
+    price: Mapped[int]
+
+    @hybrid_property
+    def total_cost(self) -> int:
+        days = (self.date_to - self.date_from).days
+        return days * self.price
+    
