648ec64f47fb9d67fcf3904778e6cb36b08d24be
DoonyFreeman <rebrikov2006@gmail.com>
2026-05-02 22:55:22 +0300

    bug fixed

--- src/api/images.py
+++ src/api/images.py
@@ -11,7 +11,6 @@ def upload_image(file: UploadFile, background_tasks: BackgroundTasks):
     image_path = f"src/static/images/{file.filename}"
     with open(f"src/static/images/{file.filename}", "wb+") as new_file:
         shutil.copyfileobj(file.file, new_file)
-        return {"status": "OK"}
 
     # resize_image.delay(image_path)
     background_tasks.add_task(resize_image, image_path)
