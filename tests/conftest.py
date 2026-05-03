de7f8db1e6f5437928d4def7fb31fee92b876880
DoonyFreeman <rebrikov2006@gmail.com>
2026-05-03 19:21:04 +0300

    added data for test hotels api

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
 import pytest
 from httpx import AsyncClient
+import json
 
 from src.config import settings
 from src.main import app
-from src.database import Base, engine_null_pool
+from src.database import Base, engine_null_pool, async_session_maker_null_pool
 from src.models import *
-
+from src.schemas.hotels import HotelAdd
+from src.schemas.rooms import RoomAdd
+from src.utils.db_manager import DBManager
 
 @pytest.fixture(scope="session", autouse=True)
 def check_test_mode():
@@ -18,6 +21,18 @@ async def setup_database(check_test_mode):
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
+    with open("tests/mock_hotels.json", encoding="utf-8") as file_hotels:
+        hotels = json.load(file_hotels)
+    with open("tests/mock_rooms.json", encoding="utf-8") as file_rooms:
+        rooms = json.load(file_rooms)
+
+    hotels = [HotelAdd.model_validate(hotel) for hotel in hotels]
+    rooms = [RoomAdd.model_validate(room) for room in rooms]
+
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        await db.hotels.add_bulk(hotels)
+        await db.rooms.add_bulk(rooms)
+        await db.commit()
 
 @pytest.fixture(scope="session", autouse=True)
 async def register_user(setup_database):
