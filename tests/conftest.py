636c14dac47ec559084804206a2419d0af5c2acb
DoonyFreeman <rebrikov2006@gmail.com>
2026-05-03 18:47:17 +0300

    added test user as fixture and fixed bugs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,31 @@
 import pytest
+from httpx import AsyncClient
 
-from sqlalchemy.ext.asyncio import create_async_engine
 from src.config import settings
+from src.main import app
 from src.database import Base, engine_null_pool
 from src.models import *
 
-@pytest.fixture(scope="session", autouse=True)
-async def async_main():
-    assert settings.MODE=="TEST"
 
+@pytest.fixture(scope="session", autouse=True)
+def check_test_mode():
+    assert settings.MODE == "TEST"
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def setup_database(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(setup_database):
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        await ac.post(
+            "/auth/register",
+            json={
+                "email": "kot@pes.com",
+                "password": "1234"
+            }
+        )
