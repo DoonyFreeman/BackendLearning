86d0e3559f150ab308900446a3f89b52fd1a7426
DoonyFreeman <rebrikov2006@gmail.com>
2026-05-03 16:35:26 +0300

    added test db with fixture

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,4 @@ from src.models.hotels import HotelsOrm
 from src.models.rooms import RoomsOrm
 from src.models.bookings import BookingsOrm
 from src.models.facilities import FacilitiesOrm, RoomsFacilitiesOrm
+from src.models.users import UsersOrm
