c6cfdff707edd2848e2ec673393034fc337c0cf0
DoonyFreeman <rebrikov2006@gmail.com>
2026-05-05 16:18:20 +0300

    added fixture with authenticated user

--- tests/conftest.py
+++ tests/conftest.py
@@ -69,3 +69,15 @@ async def register_user(ac, setup_database):
         }
     )
 
+
+@pytest.fixture(scope="session")
+async def authenticated_ac(register_user, ac):
+    await ac.post(
+        "/auth/login",
+        json={
+            "email": "kot@pes.com",
+            "password": "1234"
+        }
+    )
+    assert ac.cookies.get["access_token"]
+    yield ac
